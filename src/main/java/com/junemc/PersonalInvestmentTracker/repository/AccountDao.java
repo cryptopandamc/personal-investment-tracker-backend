368b6c5e42f86f8f13462d8fc2720a7ff47f25d2
cryptopandamc <junetefl@gmail.com>
2021-01-20 14:58:13 +0000

    removes unnecessary code from DAO

--- src/main/java/com/junemc/PersonalInvestmentTracker/repository/AccountDao.java
+++ src/main/java/com/junemc/PersonalInvestmentTracker/repository/AccountDao.java
@@ -1,20 +1,11 @@
 package com.junemc.PersonalInvestmentTracker.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.junemc.PersonalInvestmentTracker.model.Account;
 
 public interface AccountDao extends JpaRepository<Account, Long> {
 	
-	/*
-	public default  Optional<Account> update(Account account){
-		if (this.findById(account.getAccountId()).isEmpty()) {
-			return Optional.empty();
-		}
-		return Optional.ofNullable(account);
-	}
-	*/
+	
 
 }
